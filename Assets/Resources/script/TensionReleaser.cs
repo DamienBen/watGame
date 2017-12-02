eabc7f93766d7180c837b87225bc3b813e158106
Florian Pigis <florian.pigis@gmail.com>
2017-12-02 11:47:31 +0100

    player init

---
+++ Assets/Resources/script/TensionReleaser.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TensionReleaser : MonoBehaviour
+{
+	void OnTriggerStay (Collider col)
+	{
+		if (col.gameObject.tag == "Tension") {
+			SpriteRenderer renderer = GetComponent<SpriteRenderer> ();
+			renderer.color = new Color (0f, 1f, 0f, 1f);
+
+			if (Input.GetKey ("space")) {
+				Rage rage = col.gameObject.GetComponent<Rage> ();
+				rage.releaseTension ();
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+		renderer.color = new Color(1f, 1f, 1f, 1f);
+	}
+}
