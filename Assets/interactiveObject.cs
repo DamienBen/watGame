cf80917a2e842f599327cac46df99515e84f1b1c
PCF-156 <comptoirs@PCF-156.cdm.local>
2017-12-02 13:07:21 +0100

    pathfinding

---
+++ Assets/interactiveObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactiveObject : MonoBehaviour {
+
+	//Define Enum
+     public enum TypeEnum{Test1, Test2, Test3};
+     
+     //This is what you need to show in the inspector.
+     public TypeEnum Tests;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	TypeEnum getType(){
+		return Tests;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
